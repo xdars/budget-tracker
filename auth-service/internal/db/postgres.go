c8906a18ab9bc65fbec98dac3d5adb55b7ea1339
alien <alien@lm.local>
2025-05-30 10:55:15 +0300

    Add internal/postgres.go

---
+++ auth-service/internal/db/postgres.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"database/sql"
+	"log"
+	"os"
+	"time"
+)
+
+func SetupPostgresConn() *sql.DB {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Failed to load .env\n")
+		return nil
+	}
+
+	connString := os.Getenv("POSTGRES_URL")
+
+	db, err := sql.Open("postgres", connString)
+
+    db.SetMaxOpenConns(25)
+    db.SetMaxIdleConns(25)
+    db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Postgres connection OK")
+
+	return db
+}
+
+
